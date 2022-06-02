26d7fa254a3de3c0d54de723f8d9d8e8c47756d1
HehexOne <werty.mn2@yandex.ru>
2022-06-02 21:16:58 +0300

    fixed header at product.php

--- product.php
+++ product.php
@@ -65,8 +65,8 @@ $product_in_cart = getQuantity($product_id);
              id="navbarNavAltMarkup">
             <ul class="navbar-nav">
                 <li class="nav-item"><a class="nav-link" aria-current="page" href="index.php">Каталог</a></li>
-                <li class="nav-item"><a class="nav-link" href="tracking.php">Отслеживание</a></li>
                 <li class="nav-item"><a class="nav-link" href="about.php">Контакты</a></li>
+                <li class="nav-item"><a class="nav-link" href="tracking.php">Отслеживание</a></li>
             </ul>
             <ul class="pagination mt-3">
                 <li class="page-item disabled">
